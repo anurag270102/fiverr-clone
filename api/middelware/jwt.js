8952fefea471b7c8a182d8bba1a13396cef77770
anurag270102 <anuragdalsaniya@gmail.com>
2026-06-24 22:11:52 +0530

    [Feature] implement JWT refresh token functionality and enhance authentication flow

--- api/middelware/jwt.js
+++ api/middelware/jwt.js
@@ -1,14 +1,14 @@
-import Jwt from 'jsonwebtoken';
-import createError from '../utils/createError.js';
-export const verifyToken=async(req,res,next)=>{
-    
-    const token = req.cookies.accessToken;
-    if (!token) return next(createError(401,'you are not authcaticated'));
-    
-    Jwt.verify(token, process.env.JWT_KEY, async (err, payload) => {
-        if (!token) return next(createError(403,'token is not valid'));
-        req.userId=payload.id;
-        req.isSeller=payload.isSeller;
-        next()//go to deleteUser
-    })
-}
+import Jwt from "jsonwebtoken";
+import createError from "../utils/createError.js";
+export const verifyToken = async (req, res, next) => {
+  const token = req.cookies.accessToken;
+  if (!token) return next(createError(401, "Not authenticated"));
+
+  Jwt.verify(token, process.env.JWT_KEY, (err, payload) => {
+    if (err) return next(createError(401, "Token expired"));
+
+    req.userId = payload.id;
+    req.isSeller = payload.isSeller;
+    next();
+  });
+};
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,8 +1,9 @@
 import express from "express";
-import {register,login,logout} from '../controller/auth.controller.js'
+import {register,login,logout,refreshToken} from '../controller/auth.controller.js'
+
 const router =express.Router();
 router.post('/register',register);
 router.post('/login',login);
 router.post('/logout',logout);
-
+router.post('/refresh', refreshToken);
 export default router;
